07e20cb19850f1fb9ee7aa8a7d94c8b01d164e2f
douviz89 <learnnighdie@gmail.com>
2019-01-30 22:21:11 +0100

    modif sans imapct

--- src/app/book/book.service.ts
+++ src/app/book/book.service.ts
@@ -1,15 +1,21 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+  HttpResponse
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
+
+
 
 import { Book } from './book';
 
 const getBooksUrl = 'http://localhost:8080/bookapi/api/';
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type':  'application/json',
-    'responseType': 'text'
+    'Content-Type': 'application/json'
   })
 };
 
@@ -17,51 +23,48 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class BookService {
-
-
-  constructor(private _httpService: HttpClient) {
-  }
+  constructor(private _httpService: HttpClient) {}
 
   private extractData(res: Response) {
     let body = res;
-    return body || { };
+    return body || {};
   }
 
   getAllBooks(): Observable<any> {
-    return this._httpService.get(getBooksUrl + 'books')
-              .pipe(
-                map(this.extractData),
-                catchError(this.handleError('getHeroes', []))
-              );
+    return this._httpService.get(getBooksUrl + 'books').pipe(
+      map(this.extractData),
+      // catchError(this.handleError('getHeroes', []))
+      catchError(this.handleError)
+    );
   }
 
-  addBook(book: Book): Observable<Book> {
-    return this._httpService.post<Book>(getBooksUrl + 'save', book, httpOptions)
-      .pipe(
-        catchError(this.handleError('addBook', book))
-      );
+  addBook(book: Book) {
+    let body = JSON.stringify(book);
+    return this._httpService
+      .post(getBooksUrl + 'save', body, httpOptions)
+      //.pipe(catchError(this.handleError('addBook', book)));
+      .pipe(catchError(this.handleError));
   }
-
-
-
 
   /**
- * Handle Http operation that failed.
- * Let the app continue.
- * @param operation - name of the operation that failed
- * @param result - optional value to return as the observable result
- */
-private handleError<T> (operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  // private handleError<T>(operation = 'operation', result?: T) {
+  //   return (error: any): Observable<T> => {
+  //     // TODO: send the error to remote logging infrastructure
+  //     console.error(error); // log to console instead
 
-    // TODO: send the error to remote logging infrastructure
-    console.error(error); // log to console instead
+  //     // TODO: better job of transforming error for user consumption
+  //     console.log(`${operation} failed: ${error.message}`);
 
-    // TODO: better job of transforming error for user consumption
-    console.log(`${operation} failed: ${error.message}`);
-
-    // Let the app keep running by returning an empty result.
-    return of(result as T);
-  };
-}
+  //     // Let the app keep running by returning an empty result.
+  //     return of(result as T);
+  //   };
+  // }
+  private handleError(error: Response) {
+    return throwError(error);
+  }
 }
